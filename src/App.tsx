df211e801f97a25f6d132a6e17e07f528c364a9c
jadedResearcher <gtg287y@gmail.com>
2021-04-18 18:53:00 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import {all_aspects, initAspects} from "./Modules/Aspect";
 import { all_classes, initClasses } from "./Modules/RPGClass";
-import { Skill } from "./Modules/Skill";
 import {initThemes} from "./Modules/Theme";
 import SeededRandom from "./Utils/SeededRandom";
 import {Player} from "./Modules/Player";
@@ -20,10 +19,13 @@ function App() {
     <StatusScreen player={player}></StatusScreen>
     Fast TODO
     <ul>
-      <li>abilityt to player skills and turn it into a directed acylcic graph</li>
-      <li>ability to visualize said graph</li>
-      <li>player object (has stats, skills, etc) (stats are just from wigglersim)</li>
+      <li>ability to display player skills as graph (tweaking tier if needed)</li>
+      <li>only display unlocked skills and skills adjacent to them</li>
+      <li>can click on skill to unlock it</li>
+      <li>player has skill points, can only click on skill if you can afford it ("are you sure" popup)</li>
+      <li>player stats (stats are just from wigglersim but with lists of names)</li>
       <li>special skill types that unlock other stat pages</li>
+      <li>sassy achievement system</li>
     </ul>
     </div>
   );
--- src/Screens/Status.tsx
+++ src/Screens/Status.tsx
@@ -12,42 +12,44 @@ export const  StatusScreen = (props: StatusProps)=> {
 
     return (
     <StatusBlock>
-        <StatusRow>
-            <StatusHeader>Name:</StatusHeader>
-             <StatusContent>Unknown (make this editable once)</StatusContent>
-        </StatusRow>
+        <tbody>
+            <StatusRow>
+                <StatusHeader>Name:</StatusHeader>
+                <StatusContent>Unknown (make this editable once)</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Title:</StatusHeader>
-             <StatusContent>{props.player.class_name.chosen_name} of {props.player.aspect.chosen_name}</StatusContent>
-        </StatusRow>
+            <StatusRow>
+                <StatusHeader>Title:</StatusHeader>
+                <StatusContent>{props.player.class_name.chosen_name} of {props.player.aspect.chosen_name}</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Sub Titles:</StatusHeader>
-             <StatusContent> {props.player.interests.map((interest)=>interest.chosen_name).join(",")}</StatusContent>
-        </StatusRow>
+            <StatusRow>
+                <StatusHeader>Sub Titles:</StatusHeader>
+                <StatusContent> {props.player.interests.map((interest)=>interest.chosen_name).join(",")}</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Species:</StatusHeader>
-             <StatusContent>Unknown TODO</StatusContent>
-        </StatusRow>
+            <StatusRow>
+                <StatusHeader>Species:</StatusHeader>
+                <StatusContent>Unknown TODO</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Stats:</StatusHeader>
-             <StatusContent>Unknown TODO</StatusContent>
-        </StatusRow>
+            <StatusRow>
+                <StatusHeader>Stats:</StatusHeader>
+                <StatusContent>Unknown TODO</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Themes:</StatusHeader>
-             <StatusContent>{props.player.theme_keys.join(",")}</StatusContent>
-        </StatusRow>
+            <StatusRow>
+                <StatusHeader>Themes:</StatusHeader>
+                <StatusContent>{props.player.theme_keys.join(",")}</StatusContent>
+            </StatusRow>
 
-        <StatusRow>
-            <StatusHeader>Unlocked Skills:</StatusHeader>
-             <StatusContent>   
-                  {props.player.unlocked_skills().map((skill,i) => {return ( <div>Skill {i}: {skill.name} (Rank {skill.tier}): Themes: [{skill.theme_keys.join(",")}]</div>  )})}
-      </StatusContent>
-    </StatusRow>
+            <StatusRow>
+                <StatusHeader>Unlocked Skills:</StatusHeader>
+                <StatusContent>   
+                    {props.player.unlocked_skills().map((skill,i) => {return ( <div key={skill.name + i}>Skill {i}: {skill.name} (Rank {skill.tier}): Themes: [{skill.theme_keys.join(",")}]</div>  )})}
+                </StatusContent>
+            </StatusRow>
+    </tbody>
 
 
 
