e4b4bdddb8e155b0d7f4da52742e04707bc86062
JR <jadedResearcher@gmail.com>
2021-05-12 23:10:46 -0400

    done for now

--- src/Modules/SkillGenerationAlgorithms/BonesFirstAlg.tsx
+++ src/Modules/SkillGenerationAlgorithms/BonesFirstAlg.tsx
@@ -3,7 +3,7 @@ import { Aspect } from "../Aspect"
 import { Interest } from "../Interest"
 import { RPGClass } from "../RPGClass"
 import { CoreSkill, Skill } from "../Skill"
-import { Theme } from "../Theme"
+import { all_themes, Theme } from "../Theme"
 import { SkillGenAlg } from "./SkillGenAlg"
 
 //generates the structure of the graph first, then fills it in
@@ -36,33 +36,46 @@ export   class  BonesFirstAlg extends SkillGenAlg{
         //this should happen TWICE. ~MAYBE~ three times.
         //skip root for this. (slice)
         console.log("JR NOTE: after generate root and base ret is", ret)
-        const tierTwo = this.generateTierTwo(ret[0], ret.slice(1), themes, rand);
+        const tierTwo = this.generateTierTwo(ret.slice(1), themes, rand);
         console.log("JR NOTE: tier two is ", tierTwo)
         ret = ret.concat(tierTwo);
         
         console.log("JR NOTE: after generateTierTwo ret is", ret)
+
+        const tierThree = this.generateTierTwo(tierTwo, themes, rand);
+        console.log("JR NOTE: tier two is ", tierThree)
+        ret = ret.concat(tierThree);
+        
+        console.log("JR NOTE: after generateTierThree ret is", ret)
 
         return this.only_leave_unique_names(ret);
     }
 
     // there are three nodes from "Summon Plant".  for each node generate a new skill that has plant as a theme and 0-1 others from the players theme list
 
-    generateTierTwo = (root: Skill, skills: Skill[], themes: Theme[], rand: SeededRandom) =>{
+    generateTierTwo = (skills: Skill[], themes: Theme[], rand: SeededRandom) =>{
         console.log("JR NOTE: generating tier 2");
 
-        const max = 3;
-        const min = 1;
+
         let ret:Skill[] = [];
-        for(const skill of skills){
-            const theme = rand.getRandomElementFromArray(themes);
-            if(theme.name !== skill.theme_keys[0]){
-                let tmp = (this.generate_skill_x_times(rand.getRandomNumberBetween(min,max),[theme], rand));
+        for(let i = 0; i<skills.length; i++){
+            //combine a skill with its neighbor, loop if necessary
+            const skill = skills[i];
+            const second_skill = i<skills.length-1? skills[i+1]:skills[0];
+            console.log("JR NOTE: combining", skill.name, "with", second_skill.name);
+            const theme1 = all_themes[rand.getRandomElementFromArray(second_skill.theme_keys)];
+            const theme2 = all_themes[rand.getRandomElementFromArray(skill.theme_keys)];
+
+            if(theme1.key !== theme2.key){
+                let tmp = (this.generate_skill_x_times(2,[theme1, theme2], rand));
                 tmp = this.only_leave_unique_names(tmp);
                 for(const new_skill of tmp){
                     //no doubles
                     if(ret.filter((s) =>s.name === new_skill.name).length ===0){
                         ret.push(new_skill);
                         this.assignChild(skill, new_skill);
+                        this.assignChild(second_skill, new_skill);
+
                     }
                 }
 
