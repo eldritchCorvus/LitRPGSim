eee7c8cf7b432abb3134b944b72bdcbcdffc2e86
JR <jadedResearcher@gmail.com>
2021-06-05 13:21:46 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/App.tsx
+++ src/App.tsx
@@ -62,10 +62,12 @@ function App() {
 
       Fast TODO (yeah)
       <ul>
+        <li>secondary titles pick an adjactive from your theme list to pair? something either saved or stable. Blazing Knight, etc.</li>
         <li>have it so any time the player does ANYTHING it records it and checks for achivements</li>
         <li>record every time you hit ESC as an error, make up a convincing error about menu.close not found.</li>
         <li>if errors build up enough, swap above and below text, regenerate skills filled with insults instead of nouns or adjs</li>
         <li>achivements award skill points</li>
+        <li>sub titles unlock only when you unlock skills related to them. you have to discover them, essentially</li>
         <li>implement more achivment trigger types</li>
         <li>actually compile this and put an experimental version on the web</li>
         <li>store hacking methods in the window.</li>
--- src/Modules/Interest.tsx
+++ src/Modules/Interest.tsx
@@ -1,6 +1,7 @@
 import SeededRandom from '../Utils/SeededRandom';
 import {Theme, all_themes} from "./Theme";
 import * as Stat from './Stat';
+import { ADDICTION, ANGER, BUGS, CHOICES, CLOWNS, DARKNESS, DECAY, DEFENSE, DOLLS, ENDINGS, GUIDING, HEALING, KILLING, LOVE, MUSIC, OBFUSCATION, PLANTS, QUESTING, ROYALTY, SOUL, SPYING, WEB, ZAP } from './ThemeStorage';
 
 interface InterestMap {
     [details: string] : Interest;
@@ -50,6 +51,33 @@ export  class Interest{
 
 export function initInterests(seeded_random: SeededRandom){
     new Interest("crafting", ["Crafter", "Creator", "Blue-collarly Pursuer", "Omnismith"], seeded_random,Stat.WrapStatsToStatMap([Stat.SPACE(1), Stat.HOPE(1)]), [all_themes.crafting]);
-    new Interest("language", ["Communicator", "Language Lover", "Speaker", "Writer", "Scribe", "Author","Rune Researcher"], seeded_random, Stat.WrapStatsToStatMap([Stat.SPACE(1), Stat.LIFE(1)]),[all_themes.language]);
+    new Interest("language", ["Communicator", "Language Lover", "Speaker", "Writer", "Scribe", "Author","Rune Researcher"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(1), Stat.MIND(1)]),[all_themes.language]);
+    new Interest("gardening", ["Gardener", "Plantsmith", "Farmer", "Horticulturist", "Groundskeeper", "Druid","Landscaper"], seeded_random, Stat.WrapStatsToStatMap([Stat.SPACE(1), Stat.LIFE(1)]),[all_themes[PLANTS]]);
+    new Interest("gambling", ["Gambler", "Dealer", "Chemist", "Cardsmith", "High Roller"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(2)]),[all_themes[ADDICTION]]);
+    new Interest("spying", ["Spy", "Watcher", "Observer", "Secret Agent"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(1),Stat.MIND(1)]),[all_themes[SPYING]]);
+    new Interest("healing", ["Doctor", "Healer", "Nurse", "White Mage"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIFE(2)]),[all_themes[HEALING]]);
+    new Interest("child", ["Child", "Nursmaid", "Nanny", "Babysitter","Doll","Mannequin","Figure"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIFE(1),Stat.HOPE(1)]),[all_themes[DOLLS]]);
+    new Interest("mystery", ["Mystery", "Secret", "Unkown", "[REDACTED]"], seeded_random, Stat.WrapStatsToStatMap([Stat.VOID(2)]),[all_themes[OBFUSCATION]]);
+    new Interest("edgelord", ["Edgelord", "Ninja", "Shadow Stalker", "Nightwatch"], seeded_random, Stat.WrapStatsToStatMap([Stat.VOID(2)]),[all_themes[DARKNESS]]);
+    new Interest("killing", ["Assasin", "Murderer", "Killer"], seeded_random, Stat.WrapStatsToStatMap([Stat.RAGE(2)]),[all_themes[KILLING]]);
+    new Interest("musician", ["Singer", "Danger", "Musician","Bard","Performer"], seeded_random, Stat.WrapStatsToStatMap([Stat.RAGE(1),Stat.HOPE(1)]),[all_themes[MUSIC]]);
+    new Interest("knight", ["Knight", "Soldier", "Protector","Paladin","Warrior","Guardian"], seeded_random, Stat.WrapStatsToStatMap([Stat.HOPE(2)]),[all_themes[DEFENSE]]);
+    new Interest("seeker", ["Seeker", "Searcher", "Questor","Traveler","Adventurer"], seeded_random, Stat.WrapStatsToStatMap([Stat.HOPE(2)]),[all_themes[QUESTING]]);
+    new Interest("entymologist", ["Bug Lover", "Bug Catcher", "Entymologist"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIFE(2)]),[all_themes[BUGS]]);
+    new Interest("guide", ["Guide", "Sherpa", "Helper","Teacher"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(1), Stat.MIND(1)]),[all_themes[GUIDING]]);
+    new Interest("scholar", ["Scholar", "Sage", "Scientist","Academic","Polymath","Intellectual"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(1), Stat.MIND(1)]),[all_themes[GUIDING]]);
+    new Interest("endings", ["Ender", "Finale", "Conductor","Prophet"], seeded_random, Stat.WrapStatsToStatMap([Stat.DOOM(2)]),[all_themes[ENDINGS]]);
+    new Interest("royal", ["Prince", "Princess","King", "Queen","Royal","Ruler"], seeded_random, Stat.WrapStatsToStatMap([Stat.HOPE(2)]),[all_themes[ROYALTY]]);
+    new Interest("spider", ["Weaver", "Spinner", "Puppeteer","Spider","Puppet-Master"], seeded_random, Stat.WrapStatsToStatMap([Stat.MIND(1),Stat.DOOM(1)]),[all_themes[WEB]]);
+    new Interest("anger", ["Beserker", "Rebel", "Hater"], seeded_random, Stat.WrapStatsToStatMap([Stat.RAGE(2)]),[all_themes[ANGER]]);
+    new Interest("soul", ["Soul", "Gem", "Heart","Core"], seeded_random, Stat.WrapStatsToStatMap([Stat.HEART(2)]),[all_themes[SOUL]]);
+    new Interest("lover", ["Lover", "Partner", "Paramour","Sweetheart"], seeded_random, Stat.WrapStatsToStatMap([Stat.HEART(2)]),[all_themes[LOVE]]);
+    new Interest("electrician", ["Electrician", "Power Plant", "Engineer","Spark"], seeded_random, Stat.WrapStatsToStatMap([Stat.MIND(2)]),[all_themes[ZAP]]);
+    new Interest("chooser", ["Chooser", "Decision-Maker", "Administrator"], seeded_random, Stat.WrapStatsToStatMap([Stat.MIND(2)]),[all_themes[CHOICES]]);
+    new Interest("decay", ["Zombie", "Corpse", "Ghoul","Lich","Undertaker","Mortician"], seeded_random, Stat.WrapStatsToStatMap([Stat.DOOM(2)]),[all_themes[DECAY]]);
+    new Interest("clown", ["Clown", "Mime", "Ringmaster","Acrobat","Jester","Harlequin"], seeded_random, Stat.WrapStatsToStatMap([Stat.RAGE(2)]),[all_themes[CLOWNS]]);
+
+    /*
+export const CLOWNS = "clowns";*/
 
 }
