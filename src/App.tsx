00ceb5d0e226c79a83bf832d2ff1bd9b7c2cd6be
JR <jadedResearcher@gmail.com>
2021-06-08 20:17:24 -0400

    bones of rage mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import {initClasses } from "./Modules/RPGClass";
 import {initThemes} from "./Modules/Theme";
 import SeededRandom from "./Utils/SeededRandom";
 import {Player, randomPlayer} from "./Modules/Player";
-import {StatusScreen} from "./Screens/Status";
-import {LoadingScreen} from "./Screens/Loading";
-import {SkillGraphScreen} from "./Screens/SkillsGraph";
 import {initInterests } from "./Modules/Interest";
 import React, {useEffect, useState,Fragment} from 'react';
-import {STATUS, LOADING, SKILLGRAPH, ACHIEVEMENTS, STATISTICS} from "./Utils/constants";
 import { initStats } from "./Modules/Stat";
 import { getRandomSeed, numbertoseed } from "./Utils/NonSeededRandUtils";
 import { getParameterByName } from "./Utils/URLUtils";
-import { useTabState, Tab, TabList, TabPanel } from "reakit/Tab";
-import { MenuBox } from "./Screens/Styles";
-import { StatisticsScreen } from "./Screens/Statisics";
-import { AchivementsScreen } from "./Screens/Achivements";
+import Menu from "./Menu";
+import RageMode from "./RageMode";
+
 
 
 const selectedTab = {
@@ -35,10 +30,9 @@ const unSelectedTab = {
 }
 
 function App() {
-  //order matters, themes are needed for aspects, etc;
-  const [currentScreen, setCurrentScreen] = useState(LOADING);
-  const [nextScreen, setNextScreen] = useState(STATUS);
   const [player, setPlayer] = useState<Player>();
+  const [rageMode, setRageMode] = useState(false);
+
 
   useEffect(()=>{
     if(!player){
@@ -51,7 +45,8 @@ function App() {
       if(!initial_seed){
         initial_seed= getRandomSeed();
       }
-      const rand = new SeededRandom(initial_seed);
+      const rand = new SeededRandom(initial_seed);  
+      //order matters, themes are needed for aspects, etc;
       initStats();
       initThemes();
       initAspects(rand);
@@ -61,20 +56,7 @@ function App() {
       setPlayer(randomPlayer(rand) ); 
   }
   },[player])
-
-
-  const tab = useTabState();
-
-  //screen and tab should stay in sync plz
-  useEffect(()=>{
-    tab.setSelectedId(currentScreen);
-  }, [currentScreen])
-
-  const handleLoading = (screen:string) =>{
-    setNextScreen(screen);
-    setCurrentScreen(LOADING);
-  }
-  /*
+/*
   The Layers:
 
 * A normal RPG :) :) :)
@@ -90,79 +72,17 @@ function App() {
   }else{
 
     return (
-      <div>
-        <div>TODO design this better</div>
-        <a className="link" onClick={() => {
-          //warning: since these are async this might not be the best idea.
-          setNextScreen(STATUS);
-          setCurrentScreen(LOADING)}
-          }>STATUS</a> | 
-        <a className="link" onClick={() =>{
-          setNextScreen(SKILLGRAPH);
-      setCurrentScreen(LOADING)}
-      }>SKILLS</a>
-
-      <MenuBox>
-        {
-          currentScreen === LOADING?
-            <LoadingScreen loadScreen={setCurrentScreen} nextScreen={nextScreen}></LoadingScreen>
-          :
-          <Fragment>
-            <TabList style={{borderBottom:"1px solid black"}} {...tab} aria-label="My tabs">
-              <Tab style={tab.selectedId == STATUS?selectedTab:unSelectedTab} id={STATUS} {...tab}  onClick={() => {
-          //warning: since these are async this might not be the best idea.
-          setNextScreen(STATUS);
-          setCurrentScreen(LOADING)}
-          }>Status</Tab>
-              <Tab style={tab.selectedId == SKILLGRAPH?selectedTab:unSelectedTab} id={SKILLGRAPH} onClick={() =>
-                {
-                  setNextScreen(SKILLGRAPH);
-                  setCurrentScreen(LOADING)}
-                } {...tab}>
-                Skills
-              </Tab>
-
-              <Tab style={tab.selectedId == STATISTICS?selectedTab:unSelectedTab} id={STATISTICS} onClick={() =>
-                {
-                  setNextScreen(STATISTICS);
-                  setCurrentScreen(LOADING)}
-                } {...tab}>
-                Statistics
-              </Tab>
-
-              <Tab style={tab.selectedId == ACHIEVEMENTS?selectedTab:unSelectedTab} id={ACHIEVEMENTS} onClick={() =>
-                {
-                  setNextScreen(ACHIEVEMENTS);
-                  setCurrentScreen(LOADING)}
-                } {...tab}>
-                Achivements
-              </Tab>
-            </TabList>
-            <TabPanel {...tab}>
-              {currentScreen === STATUS?<StatusScreen loadScreen={handleLoading} player={player}></StatusScreen>:null}
-            </TabPanel>
-            <TabPanel {...tab}>
-              {currentScreen === SKILLGRAPH?<SkillGraphScreen  loadScreen={handleLoading} player={player}></SkillGraphScreen>:null}
-
-            </TabPanel>
-
-            <TabPanel {...tab}>
-              {currentScreen === STATISTICS?<StatisticsScreen  loadScreen={handleLoading} player={player}></StatisticsScreen>:null}
-
-            </TabPanel>
-
-            <TabPanel {...tab}>
-              {currentScreen === ACHIEVEMENTS?<AchivementsScreen  loadScreen={handleLoading} player={player}></AchivementsScreen>:null}
-
-            </TabPanel>
-          </Fragment>
-        }
-      </MenuBox>
+      <Fragment>
+        <button onClick={()=> setRageMode(!rageMode)}>TEST RAGE MODE PLZ</button>
+        {rageMode? <RageMode/>:null}
+        <Menu player={player}/>
+      
 
       
 
       Fast TODO (yeah)
       <ul>
+        <li>RAGE MODE</li>
         <li>wire up more of the stats. why isn't achivements unlocked working? why is time started not working? display time correctly</li>
         <li>record every time you hit ESC as an error, make up a convincing error about menu.close not found.</li>
         <li>waste class, all skills are the names of hack methods on window. choosing those skills calls the method.</li>
@@ -182,7 +102,7 @@ function App() {
         <li>experiment with three themes mixed together (how would names work? be all grandiose, look at fraymotifs, maybe always add music or something at the end?)</li>
 
       </ul>
-      </div>
+      </Fragment>
     );
   }
 } 
