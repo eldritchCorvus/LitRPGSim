56fe3823006ae47f6814ca328f4c37171e98510b
JR <jadedResearcher@gmail.com>
2021-06-15 08:58:20 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/App.tsx
+++ src/App.tsx
@@ -91,22 +91,21 @@ function App() {
       Fast TODO (yeah)
       <ul style={{display: "none"}}>
 
-        <li>options, things like volume, opacity, haxMode, etc.</li>
-        <li>as waste class, if unlock skill you get popup that says "im sorry, but hacking commands from the skill graph are currently disabled", and have a secret, but on screen way to enable them. maybe in the options menu. (obvs haxMode=on)</li>
+        <li>options, things like volume, opacity/blurring, haxMode, etc.</li>
+        <li>as waste class options always in skill tree, if unlock skill you get popup that says "im sorry, but hacking commands from the skill graph are currently disabled", and have a secret, but on screen way to enable them. maybe in the options menu. (obvs haxMode=on)</li>
         <li>waste class, all skills are the names of hack methods on window. choosing those skills calls the method.</li>
         <li>calling ANY hack method that implies you've played the game (such as combat time) or having enough errors activates RAGE MODE</li>
         <li>allow literal hacking of DOM too, if they edit statistics page, slorp it up.</li>
         <li>rename skills to have insults based on themes</li>
         <li>port in fractal sim to be the bg for RAGE MODE</li>
         <li>center skills tree on last unlocked skill, or root if none</li>
-        <li>option menu with menu opacity slider (blurring effect)</li>
         <li>later, make it so if its your first playthrough (check local storage) and you get waste class it changes to something else.</li>
         <li>implement more achivment trigger types</li>
         <li>achivements award skill points (number of which is passed into achivement)</li>
         <li>sub titles unlock only when you unlock skills related to them. you have to discover them, essentially</li>
         <li>murder yellow text</li>
         <li>player has skill points, can only click on skill if you can afford it and ALL its parents are unlocked ("are you sure" popup)</li>
-        <li>skills that unlock other menu screens/upgrade them</li>
+        <li>skills that unlock other menu screens/upgrade them (replace a stat with it)</li>
         <li>zero player game where you get little mini stories about what you 'did', like "used Medical Crown to heal a king" or whatever.  if the game were working PROPERLY it should praise you for whatever skill you've used the most, but obviously you've never used a single skill so it just picks one at random or glitches out. have console logs about ERORR NO FAVORIE SKILL FOUND etc.</li>
         <li>experiment with three themes mixed together (how would names work? be all grandiose, look at fraymotifs, maybe always add music or something at the end?)</li>
 
