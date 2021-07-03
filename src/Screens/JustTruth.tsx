05afab0a586eb7b8c52a57a12071c617f7c28f3d
JR <jadedResearcher@gmail.com>
2021-07-03 12:48:56 -0400

    just Refs: #22917 mode

---
+++ src/Screens/JustTruth.tsx
@@ -0,0 +1,25 @@
+import  React, { useEffect } from "react";
+import { justTruthSong } from "..";
+import { Player } from "../Modules/Player";
+import { TruthContainer } from "./Styles";
+interface StatusProps{
+    player: Player;
+}
+export const  JustTruth = (props: StatusProps)=> {
+    const {player} = props
+    useEffect(()=>{
+        console.log("changing song");
+        justTruthSong();
+        const canvas = document.getElementById("ThisIsNotASpiral");
+        if(canvas){
+          canvas.style.opacity="0.2";
+          canvas.classList.remove("chaos");
+          canvas.classList.remove("frameAnimation");
+        }
+    },[]);
+
+    return (
+      
+        <TruthContainer>JUST TRUTH TODO INFINITE QUIZ</TruthContainer>
+  );
+  }
