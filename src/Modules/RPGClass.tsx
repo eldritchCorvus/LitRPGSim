0c3d4b287f4ea2b5d5370afd84596d5100aeb720
JR <jadedResearcher@gmail.com>
2021-07-02 23:33:19 -0400

    can even change the loudness quotient

--- src/Modules/RPGClass.tsx
+++ src/Modules/RPGClass.tsx
@@ -59,6 +59,7 @@ export  class RPGClass{
 }
 
 export function initClasses(seeded_random: SeededRandom){
+    //TODO associate each with array of MENU NAMES to be associated with. 
     new RPGClass("seer", ["Seer", "Watcher", "Guide", "Sherpa", "Eye","Observer"],1.3, seeded_random, [all_themes.knowing, all_themes.guiding]);
     new RPGClass("prince", ["Prince", "Reaper", "Destroyer", "Finisher", "Finale"],-2.0, seeded_random, [all_themes.endings, all_themes.royalty]);
     new RPGClass("bard", ["Bard", "Singer", "Skald", "Teller", "Raconteur"],-2.0, seeded_random, [all_themes.clowns, all_themes.language,all_themes.music]);
--- src/Utils/MusicPlayer.ts
+++
@@ -1,5 +0,0 @@
-export function playSong(song:string){
-    console.log("trying to play: ",song)
-    const audio = new Audio(song);
-    audio.play();
-}
