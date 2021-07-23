6436384a4df361fe3bdb710e0f24ddf2332a3fc3
JR <jadedResearcher@gmail.com>
2021-07-22 20:00:02 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

---
+++ src/AppWrapper.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import About from "./About";
+import App from "./App";
+import Birthday from "./Birthday";
+import { getRandomSeed } from "./Utils/NonSeededRandUtils";
+import { isNumeric, stringtoseed } from "./Utils/StringUtils";
+import { getParameterByName } from "./Utils/URLUtils";
+/*
+as simple as possible, handles the three main screens of "enter your birthday", 
+"play the game", "jr rambles about dev log shit"
+maybe an "about" page too
+four then.
+*/
+
+export const BIRTHDAY = "BIRTHDAY";
+export const ABOUT = "ABOUT";
+//game mode is just "is there a seed";
+
+function AppWrapper() {
+  const [seed, setSeed] = useState<number>();
+  const [mode, setMode] = useState<string>(BIRTHDAY);
+
+  useEffect(()=>{
+    if(!seed){
+      let initial_seed;
+      let urlseed:string|number|null = getParameterByName("seed",null);
+      if (urlseed){
+        if(!isNumeric(urlseed)){
+            initial_seed = stringtoseed(urlseed);
+        }else{
+          initial_seed = parseInt(urlseed);
+        }
+        console.log("initial_seed seed is", initial_seed);
+        setSeed(initial_seed);
+      }
+    }
+  },[seed, setSeed]);
+
+  if(seed){
+    return(
+      <App seed={seed}/>
+    )
+  }else if(mode == BIRTHDAY){
+    return(
+      <Birthday setMode={setMode}/>
+    )
+  }else if (mode == ABOUT){
+    return(
+      <About setMode={setMode}/>
+    )
+  }
+} 
+
+export default AppWrapper;
+
