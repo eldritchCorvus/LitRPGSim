5c587a416098fee3db7da38f7c5f22f2ff466375
JR <jadedResearcher@gmail.com>
2021-07-24 19:26:31 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/Modules/Aspect.tsx
+++ src/Modules/Aspect.tsx
@@ -1,7 +1,7 @@
 import SeededRandom from '../Utils/SeededRandom';
 import * as Stat from './Stat';
 import {Theme, all_themes} from "./Theme";
-import { ANGELS, APOCALYPSE, BURIED, DARKNESS, DEATH, ENDINGS, FAMILY, FIRE, FLESH, FOG, FREEDOM, HUNTING, KILLING, KNOWING, LIGHT, LONELY, OBFUSCATION, OCEAN, QUESTING, SOUL, STEALING, TWISTING } from './ThemeStorage';
+import { ANGELS, APOCALYPSE, BURIED, DARKNESS, DEATH, ENDINGS, FAMILY, FIRE, FLESH, FREEDOM, HUNTING, KILLING, KNOWING, LIGHT, LONELY, MUSIC, OBFUSCATION, OCEAN, PLANTS, QUESTING, SOUL, SPACE, STEALING, TWISTING } from './ThemeStorage';
 //TODO aspects also have hardcoded skills (with or without themes)
 //some skills are special purpose, for example, the ability to see/upgrade the status screen
 //or see/upgrade your allies (blood would especially be good at this)
@@ -80,6 +80,8 @@ export function initAspects(seeded_random: SeededRandom){
     new Aspect("light", ["Light", "Knowledge", "Attention", "Exposition"], seeded_random, Stat.WrapStatsToStatMap([Stat.LIGHT(3)]), [all_themes[KNOWING],all_themes[LIGHT]]);
     new Aspect("blood", ["Blood", "Bonds", "Family", "Chains","Connections","Fellowship","Ties","Union"], seeded_random, Stat.WrapStatsToStatMap([Stat.BLOOD(3)]), [all_themes[KILLING],all_themes[FAMILY]]);
     new Aspect("breath", ["Breath", "Wind", "Freedom", "Independence","Autonomy","Breeze","Travel","Liberty"], seeded_random, Stat.WrapStatsToStatMap([Stat.BREATH(3)]), [all_themes[FREEDOM],all_themes[LONELY]]);
+    new Aspect("space", ["Vast", "Space","Air", "Expanse", "Volume","Position","Falling","Setting"], seeded_random, Stat.WrapStatsToStatMap([Stat.SPACE(3)]), [all_themes[SPACE], all_themes[PLANTS]]);
+    new Aspect("time", ["Time", "Clocks","Tempo", "Pacing", "Measure","Beat","Cadence","Stress"], seeded_random, Stat.WrapStatsToStatMap([Stat.SPACE(3)]), [all_themes[ENDINGS], all_themes[MUSIC]]);
 
     new Aspect("corruption", ["Love", "Corruption", "Toxicity", "Inserts", "Plague","Rot","Disgust","Bugs","Maggots","Worms"], seeded_random, Stat.WrapStatsToStatMap([Stat.DOOM(1),Stat.LIFE(1),Stat.HEART(1)]), [all_themes.decay, all_themes.bugs,all_themes.love]);
     new Aspect("web", ["Webs", "Spiders", "Fate", "Addiction"], seeded_random, Stat.WrapStatsToStatMap([Stat.DOOM(1),Stat.VOID(1),Stat.HEART(1)]), [all_themes.addiction, all_themes.web]);
@@ -95,4 +97,7 @@ export function initAspects(seeded_random: SeededRandom){
     new Aspect("desolation", ["Desolation", "Fire", "Loss", "Blaze","Ruin","Misfortune","Calamity","Flame"], seeded_random, Stat.WrapStatsToStatMap([Stat.DOOM(1), Stat.RAGE(1)]), [all_themes[FIRE], all_themes[STEALING]]);
     new Aspect("lonely", ["Lonely", "Outcasts","Isolation", "Seclusion", "Cold","Frost","Fog","Mist","Sea"], seeded_random, Stat.WrapStatsToStatMap([Stat.BREATH(1), Stat.VOID(1)]), [all_themes[OCEAN], all_themes[LONELY], all_themes[OBFUSCATION]]);
 
+    //dark and vast are directly represented in homestuck
+
+    //TODO want to do spider's ruler skills? greed, perserverence etc
 }
