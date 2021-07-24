37ff5d61f2ceeafe8ebab723fc84b44d13f96f11
JR <jadedResearcher@gmail.com>
2021-07-24 10:25:39 -0400

    start screen complete

--- src/About.tsx
+++ src/About.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { BIRTHDAY } from "./AppWrapper";
+import { LinkButton } from "./Screens/Styles";
 
 interface AppProps{
   setMode: any; //lazy don't wanna remember setstate type
 }
 function About(props: AppProps) {
-  
+  const [date, setDate] = useState<string>("1933-03-03");
+
     return (
       <div>
         <div>TODO ABOUT PLUS DEV LOG</div>
-        <a onClick={()=>{props.setMode(BIRTHDAY)}}>Enter Zampanio</a>
+        <LinkButton onClick={() => { props.setMode(BIRTHDAY) }}>Enter Zampanio</LinkButton>
+        {date}
       </div>
     );
 } 
