502ef156f24073365134801d1785c31a8d3c01bf
JR <jadedResearcher@gmail.com>
2021-07-25 09:45:25 -0400

    wiki

--- src/Screens/JustTruth.tsx
+++ src/Screens/JustTruth.tsx
@@ -228,6 +228,8 @@ export const JustTruth = (props: StatusProps) => {
                 <div style={{height:"100px"}}>Oh. Uh. I ran out of questions. So. Shit. This wasn't supposed to end. But I don't want to risk repeating either. Here! Fine! No more masks. Not even the slightest shred of falsehoods. Just. Take it.</div>
                 <div><a target="_blank" href="https://github.com/FarragoFiction/LitRPGSim/blob/master/src/Screens/JustTruth.tsx">View My Soul 1</a></div>
                 <div><a target="_blank" href="https://github.com/FarragoFiction/LitRPGSim/blob/master/src/Modules/ObserverBot/ObserverBot.ts">View My Soul 2</a></div>
+                <div>Or. You can help the others like you navigate this Labyrinth. By which I mean: make the Labyrinth bigger.  </div>
+                <div><a target="_blank" href="https://zampaniosim.fandom.com/wiki/ZampanioSim_Wiki">View/Create My Map</a></div>
 
                 <RageStyledButton onClick={() => { setIndex(index + 1) }}> {">"} Okay?</RageStyledButton>
   
