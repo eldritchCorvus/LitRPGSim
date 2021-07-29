9aa4077f988649ed5562eba5e5353a224dcdc3ff
JR <jadedResearcher@gmail.com>
2021-07-28 22:12:45 -0400

    good little wastes will figure out how to play an actual game in zampaniosim

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import About from "./About";
 import App from "./App";
 import Birthday from "./Birthday";
-import Debug from "./Debug";
-import { getRandomSeed } from "./Utils/NonSeededRandUtils";
 import { isNumeric, stringtoseed } from "./Utils/StringUtils";
 import { getParameterByName } from "./Utils/URLUtils";
 /*
@@ -15,7 +13,6 @@ four then.
 
 export const BIRTHDAY = "BIRTHDAY";
 export const ABOUT = "ABOUT";
-export const DEBUG = "DEBUG";
 
 //game mode is just "is there a seed";
 
@@ -23,40 +20,37 @@ function AppWrapper() {
   const [seed, setSeed] = useState<number>();
   const [mode, setMode] = useState<string>(BIRTHDAY);
 
-  useEffect(()=>{
-    if(!seed){
+
+  useEffect(() => {
+    if (!seed) {
       let initial_seed;
-      let urlseed:string|number|null = getParameterByName("seed",null);
-      if (urlseed){
-        if(!isNumeric(urlseed)){
-            initial_seed = stringtoseed(urlseed);
-        }else{
+      let urlseed: string | number | null = getParameterByName("seed", null);
+      if (urlseed) {
+        if (!isNumeric(urlseed)) {
+          initial_seed = stringtoseed(urlseed);
+        } else {
           initial_seed = parseInt(urlseed);
         }
         console.log("initial_seed seed is", initial_seed);
         setSeed(initial_seed);
       }
     }
-  },[seed, setSeed]);
+  }, [seed, setSeed]);
 
-  if(seed){
-    return(
-      <App seed={seed}/>
+  if (seed) {
+    return (
+      <App seed={seed} />
     )
-  }else if(mode == BIRTHDAY){
-    return(
-      <Birthday setMode={setMode}/>
+  } else if (mode == BIRTHDAY) {
+    return (
+      <Birthday setMode={setMode} />
     )
-  }else if (mode == ABOUT){
-    return(
-      <About setMode={setMode}/>
-    )
-  }else if (mode == DEBUG){
-    return(
-      <Debug setMode={setMode}/>
+  } else if (mode == ABOUT) {
+    return (
+      <About setMode={setMode} />
     )
   }
-} 
+}
 
 export default AppWrapper;
 
