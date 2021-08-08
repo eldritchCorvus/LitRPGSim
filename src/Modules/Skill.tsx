a9c50700c24241a37fde1aaad4d7088ff5868554
JR <jadedResearcher@gmail.com>
2021-08-08 10:33:40 -0400

    tweaked dangerous rate

--- src/Modules/Skill.tsx
+++ src/Modules/Skill.tsx
@@ -138,7 +138,9 @@ export class CoreSkill extends SpecialSkill{
 
 let numStatSkills = 0;
 //todo if this gets unweildly at least put the list of functions in ObserverBot
-export const wasteHackingFunctions = ["hackStatisticsMenuLevel","hackGodsMenuLevel","hackCityBuildingMenuLevel","hackCompanionsMenuLevel","hackSkillPointsGainedFromBattle","hackCityMorale","hackStatisticsMenuLevel","hackOptionsMenuLevel","hackTimePlayedInSeconds","hackTimeCombatInSeconds","hackTimeCutscenesInSeconds","hackTimeCitybuildingInSeconds"];
+export const wasteHackingFunctions = ["hackBackstoryMenuLevel","hackInventoryMenuLevel","hackStatisticsMenuLevel","hackGodsMenuLevel","hackCityBuildingMenuLevel","hackCompanionsMenuLevel","hackStatisticsMenuLevel","hackOptionsMenuLevel","hackTimePlayedInSeconds"];
+export const dangerousWasteHackingFunctions = ["hackSkillPointsGainedFromBattle","hackCityMorale","hackTimeCombatInSeconds","hackTimeCutscenesInSeconds","hackTimeCitybuildingInSeconds"];
+
 export class WasteSkill extends SpecialSkill{
     type = "WasteSkill";
 
