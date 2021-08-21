b41a6a5b5123471045e3786446b7ebae5bf5252e
JadedResearcher <gtg287y@gmail.com>
2021-08-21 11:10:02 -0400

    they shitty irradiated bones of a cctv loop

---
+++ src/CanvasFuckery/cctv_fuckery.js
@@ -0,0 +1,8 @@
+export const cctv_loop =(canvas)=>{
+    //debug
+    const context = canvas.getContext("2d");
+
+    context.fillStyle = "red";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+}
