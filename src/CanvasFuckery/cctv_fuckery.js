5a5517009d3ba6bdeb3cb63031da3feaff37621e
JadedResearcher <gtg287y@gmail.com>
2021-08-22 11:22:06 -0400

    black and white video (need to work on contrast, and bones of dolls

--- src/CanvasFuckery/cctv_fuckery.js
+++ src/CanvasFuckery/cctv_fuckery.js
@@ -41,13 +41,13 @@ let outputCanvas;
 let image_index = 0;
 //not being functional at all, so sue me
 
-export const cctv_loop = (canvas, source,source2) => {
+export const cctv_loop = (canvas, source, source2) => {
     //debug
     //TODO make this a loop
     outputCanvas = canvas;
     sourceImage = source;
-    sourceImage2= source2;
-    possibleSourceImagePairs[source.src] =([source, source2]);
+    sourceImage2 = source2;
+    possibleSourceImagePairs[source.src] = ([source, source2]);
     // window.requestAnimationFrame()
     load_other_images();
     oneFrame(0);
@@ -55,41 +55,41 @@ export const cctv_loop = (canvas, source,source2) => {
     oneFrame(13);
 }
 
-const load_image_with_others = (next_image, images)=>{
+const load_image_with_others = (next_image, images) => {
     let img = new Image();
     const index = images.indexOf(next_image);
     img.addEventListener('load', function () {
-        possibleSourceImagePairs[next_image] = [img,img];
-        console.log("JR NOTE: image just loaded,",index,"updated possibles to ",possibleSourceImagePairs)
-        if(index <= images.length){
-         load_image_with_others(images[index+1], images);
+        possibleSourceImagePairs[next_image] = [img, img];
+        console.log("JR NOTE: image just loaded,", index, "updated possibles to ", possibleSourceImagePairs)
+        if (index <= images.length) {
+            load_image_with_others(images[index + 1], images);
         }
     }, false);
     img.src = next_image;
 }
 
 //just loads a bunch of images and when they load adds them to the array.
-const load_other_images=()=>{
+const load_other_images = () => {
     //first one is special because of the server animation
     //rest are simple.
     //two images for server blinking
     const images = [tunnel2, tunnel3, tunnel4, tunnel5];
     load_image_with_others(tunnel2, images);
-    
 
-    const click = ()=>{
+
+    const click = () => {
         const keys = Object.keys(possibleSourceImagePairs);
 
-        
-        console.log("JR NOTE: click, possible numberss are", keys.length,"with index",image_index, "and possibles of",possibleSourceImagePairs)
 
-        if(image_index < keys.length-1){
-            image_index ++;
-            console.log("JR NOTE: next image",possibleSourceImagePairs[keys[image_index]]);
-            [sourceImage,sourceImage2] = possibleSourceImagePairs[keys[image_index]];
-        }else{
+        console.log("JR NOTE: click, possible numberss are", keys.length, "with index", image_index, "and possibles of", possibleSourceImagePairs)
+
+        if (image_index < keys.length - 1) {
+            image_index++;
+            console.log("JR NOTE: next image", possibleSourceImagePairs[keys[image_index]]);
+            [sourceImage, sourceImage2] = possibleSourceImagePairs[keys[image_index]];
+        } else {
             image_index = 0;
-            [sourceImage,sourceImage2] = possibleSourceImagePairs[keys[0]];
+            [sourceImage, sourceImage2] = possibleSourceImagePairs[keys[0]];
         }
     }
 
@@ -110,12 +110,12 @@ const oneFrame = (frame) => {
     const height = sourceImage.height;
     //every other pan change direciton
     //TODO need to figure out why this isn't changing direction like i want
-    const speed =5;
-    let max =height-speed;
+    const speed = 5;
+    let max = height - speed;
 
-    let location = (frame*speed)%max;
-    if(location > height/2 ||location < 0){
-        location =  height-(frame*speed)%max;;
+    let location = (frame * speed) % max;
+    if (location > height / 2 || location < 0) {
+        location = height - (frame * speed) % max;;
     }
     //JR NOTE TODO: shadowy figures come and go, but the server should be there and blinking always
     //JR NOTE TODO: the shadowy figures should be rendered onto the image itself
@@ -123,22 +123,22 @@ const oneFrame = (frame) => {
     //because the figures need to be placed relative to the IMAGE and undernath the effects
     //(if i try to place it on the 480x480 shitty ctv they won't be in the right spot)
     let image = sourceImage2;
-    if(frame %50 <10){
+    if (frame % 50 < 10) {
         image = sourceImage;
     }
 
-    context.drawImage(image, 0, -1*location);
+    context.drawImage(image, 0, -1 * location);
     const fontSize = 25;
-    const padding = 15+fontSize;
+    const padding = 15 + fontSize;
 
     context.fillStyle = "white";
     context.font = `${fontSize}px serif`;
-    const time = getTimeString(new Date(frame*100));
+    const time = getTimeString(new Date(frame * 100));
 
     cctv(buffer, frame);
-    context.fillText(time,buffer.width-100, padding);
-    const index = 1+image_index;
-    context.fillText(`${index}`,padding, padding);
+    context.fillText(time, buffer.width - 100, padding);
+    const index = 1 + image_index;
+    context.fillText(`${index}`, padding, padding);
 
     const outputContext = outputCanvas.getContext("2d");
     outputContext.drawImage(buffer, 0, 0);
@@ -149,31 +149,31 @@ const oneFrame = (frame) => {
 //https://stackoverflow.com/questions/18229022/how-to-show-current-time-in-javascript-in-the-format-hhmmss
 function checkTime(i) {
     if (i < 10) {
-      i = "0" + i;
+        i = "0" + i;
     }
     return i;
-  }
-  
-  function getTimeString(date) {
+}
+
+function getTimeString(date) {
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
     // add a zero in front of numbers<10
     m = checkTime(m);
     s = checkTime(s);
-    return  h + ":" + m + ":" + s;
-  }
+    return h + ":" + m + ":" + s;
+}
 const isStaticSpot = (y, height, time_percent, band_width) => {
     const first = -Math.floor(height / 3 - height * time_percent);
     const second = -Math.floor(2 * height / 3 - height * time_percent);
     const third = -Math.floor(height / 4 - height * time_percent);
-    const fourth = Math.floor(0- height * time_percent)
-    const fifth =  Math.floor(height- height * time_percent)
+    const fourth = Math.floor(0 - height * time_percent)
+    const fifth = Math.floor(height - height * time_percent)
 
-    const all = [first, fourth, second, fifth,third];
-    for (let i =0; i<all.length; i++) {
+    const all = [first, fourth, second, fifth, third];
+    for (let i = 0; i < all.length; i++) {
         const item = all[i];
-        if (Math.abs(item - y) < band_width*i) {
+        if (Math.abs(item - y) < band_width * i) {
             return true;
         }
     }
@@ -201,32 +201,38 @@ const cctv = (canvas, timecode) => {
             var i = ((y * height) + x) * 4;
 
             // barred static
-            if ( staticHash(x*random_num,y*random_num2)>190&& isStaticSpot(y,height, time, 3)) {
+            if (staticHash(x * random_num, y * random_num2) > 190 && isStaticSpot(y, height, time, 3)) {
                 const value = staticHash(x + offset, y + offset);
                 d[i] = value;
                 d[i + 1] = value;
                 d[i + 2] = value;
+            } else {
+                const r = d[i];
+                const g = d[i + 1];
+                const b = d[i + 2];
+                const v = Math.max(Math.max(r,g),b);
+                d[i] = d[i + 1] = d[i + 2] = v;
             }
 
             //random static
-            if(staticHash(y*random_num,x*random_num2)>253){
-                const ratio = random_num2/255;
-                if(ratio>0.5){
-                    d[i] = 255-Math.floor(d[i]*ratio);
-                    d[i + 1] = 255-Math.floor(d[i]*ratio);
-                    d[i + 2] = 255-Math.floor(d[i]*ratio);
-                }else{
-                   const value = staticHash(x + offset, y + offset);
-                    d[i] =value;
-                    d[i + 1] = 255-value;
+            if (staticHash(y * random_num, x * random_num2) > 253) {
+                const ratio = random_num2 / 255;
+                if (ratio > 0.5) {
+                    d[i] = 255 - Math.floor(d[i] * ratio);
+                    d[i + 1] = 255 - Math.floor(d[i] * ratio);
+                    d[i + 2] = 255 - Math.floor(d[i] * ratio);
+                } else {
+                    const value = staticHash(x + offset, y + offset);
+                    d[i] = value;
+                    d[i + 1] = 255 - value;
                     d[i + 2] = value;
                 }
             }
 
             //scan lines
-            if(y%5<1){
+            if (y % 5 < 1) {
                 d[i] = 0;
-                d[i + 1] =  0;
+                d[i + 1] = 0;
                 d[i + 2] = 0;
             }
         }
