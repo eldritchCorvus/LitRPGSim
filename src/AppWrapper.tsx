3e7468a597ee887bd67242c2e824ae270e10c8af
JR <jadedResearcher@gmail.com>
2021-09-04 20:50:47 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import About from "./About";
 import App from "./App";
 import Birthday from "./Birthday";
+import { ApocalypseScreen } from "./Screens/ApocalypseScreen";
+import { CCTVScreen } from "./Screens/CCTV";
 import { isNumeric, stringtoseed } from "./Utils/StringUtils";
 import { getParameterByName } from "./Utils/URLUtils";
 /*
@@ -13,12 +15,58 @@ four then.
 
 export const BIRTHDAY = "BIRTHDAY";
 export const ABOUT = "ABOUT";
+export const APOCALYPSE = "APOCALYPSE";
+export const GHOST = "GHOST";
+export const PW = "PW";
 
 //game mode is just "is there a seed";
 
 function AppWrapper() {
   const [seed, setSeed] = useState<number>();
   const [mode, setMode] = useState<string>(BIRTHDAY);
+
+  //handle param hacks
+  useEffect (()=>{
+    const ghost = getParameterByName("cctv", null);
+    const pw = getParameterByName("pw", null);
+    const end = getParameterByName("end", null);
+
+    if(ghost){
+      (window as any).ghost = true;
+      setMode(GHOST);
+    }else if(pw){
+      (window as any).pwMode = true;
+      setMode(PW);
+    }else if(end){
+      (window as any).apocalypse = true;
+      setMode(APOCALYPSE);
+    }
+  },[]);
+  
+  //handle regular waste hacking
+  useEffect(()=>{
+    if(!(window as any).setGhostMode){
+      // :) :) :)
+      (window as any).setGhostMode = ()=>{
+        setMode(GHOST);
+      };
+    }
+
+    if(!(window as any).setPWMode){
+      // :) :) :)
+      (window as any).setPWMode = ()=>{
+        setMode(PW);
+      };
+    }
+
+    if(!(window as any).triggerApocalypse){
+      // :) :) :)
+      (window as any).triggerApocalypse = (value:boolean)=>{
+        setMode(APOCALYPSE);
+        (window as any).apocalypse = value;
+      };
+    }
+  },[])
 
 
   useEffect(() => {
@@ -48,6 +96,18 @@ function AppWrapper() {
   } else if (mode === ABOUT) {
     return (
       <About setMode={setMode} />
+    )
+  } else if (mode === APOCALYPSE) {
+    return (
+      <ApocalypseScreen/>
+    )
+  } else if (mode === GHOST) {
+    return (
+      <CCTVScreen ghosts={true}/>  
+    )
+  } else if (mode === PW) {
+    return (
+      <CCTVScreen ghosts={false}/> 
     )
   }
 }
