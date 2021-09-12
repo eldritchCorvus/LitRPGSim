cac25bf9638e59f392d99cdb812cf8765bc36f62
JR <jadedResearcher@gmail.com>
2021-09-12 15:37:41 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -233,7 +233,7 @@ function Menu(props: MenuProps) {
 
     return (
       <div id="ThisIsAMenu">
-      <MenuBox className={(window as any).real_eyes?"realeyescolor":"gaslightbgcolor"} angle={props.angle} opacity={MENU_OPACITY} mediumRadius={BORDERRADIUSROUND} fontColor={FONTCOLOR} bgColor={BGCOLOR} fontSize={FONTSIZE}>
+      <MenuBox className={(window as any).real_eyes || player.chaos?"realeyescolor":"gaslightbgcolor"} angle={props.angle} opacity={MENU_OPACITY} mediumRadius={BORDERRADIUSROUND} fontColor={FONTCOLOR} bgColor={BGCOLOR} fontSize={FONTSIZE}>
         {
           currentScreen === LOADING?
             <LoadingScreen loadScreen={setCurrentScreen} nextScreen={nextScreen} refresh={refresh}></LoadingScreen>
