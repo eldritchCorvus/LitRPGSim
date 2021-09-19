a7e6b88d0551d15f85a25e769c0f8724866e703b
JR <jadedResearcher@gmail.com>
2021-09-19 18:27:00 -0400

    1/3 dev rooms complete

--- src/Screens/Attic/LieJR.ts
+++ src/Screens/Attic/LieJR.ts
@@ -8,18 +8,30 @@
 import { JRRamble } from "./JRRamble";
 import { PlayerResponse } from "./PlayerResponse";
 
-export const makeLieRamble = ()=>{
+export const makeLieRamble = () => {
+    return One();
+}
+
+export const One = () => {
     const defaultRamble = "Lie Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     const ramble = new JRRamble(defaultRamble, []);
-    const ramble2 = new JRRamble("Did you get all that?", []);
 
-    ramble.potential_reponses.push(new PlayerResponse("Why???", ramble2));
-    ramble.potential_reponses.push(new PlayerResponse("Tell me more.", ramble2));
-    ramble.potential_reponses.push(new PlayerResponse("Why yes I DO enjoy coconu mall.", ramble2));
+    ramble.potential_reponses.push(new PlayerResponse("Why???", Two));
+    ramble.potential_reponses.push(new PlayerResponse("Tell me more.", Two));
+    ramble.potential_reponses.push(new PlayerResponse("Why yes I DO enjoy coconu mall.", Two));
 
-    ramble2.potential_reponses.push(new PlayerResponse("Because.", ramble));
-    ramble2.potential_reponses.push(new PlayerResponse("Please stop talking.", ramble));
-    ramble2.potential_reponses.push(new PlayerResponse("Wait I meant coconut.", ramble));
+
+    return ramble;
+}
+
+export const Two = () => {
+
+    const ramble = new JRRamble("Did you get all that?", []);
+
+    ramble.potential_reponses.push(new PlayerResponse("Why???", One));
+    ramble.potential_reponses.push(new PlayerResponse("Tell me more.", One));
+    ramble.potential_reponses.push(new PlayerResponse("Why yes I DO enjoy coconu mall.", One));
+
     return ramble;
 }
