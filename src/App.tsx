5911c4ab8c47808b7c2046e4f8f8619e33621952
JR <jadedResearcher@gmail.com>
2021-09-19 16:40:30 -0400

    having the DOM be a labyrnth right along with the rest of the game makes me feel so smug

--- src/App.tsx
+++ src/App.tsx
@@ -265,7 +265,7 @@ function App(props: AppProps) {
 
       Fast TODO (yeah)
       <ul style={{display: "none"}}>
-        *persephone, hades, demeter quest
+        *persephone, hades, demeter quest.  QUESTS are more "combination of side quests and overarching story plot". (think land quests)
         * session 0 is "the game is real i swear" notJR, the core of Truth is "zampanio was never real but the creepypasta faq was" notJR, and the core of game mode is "the game wasn't real and you couldn't accept that" notJR.
         *tie things into timezone like april arg
         * gaslight cursor revals a radius of true color (hidden yellow things in the pictures?)
