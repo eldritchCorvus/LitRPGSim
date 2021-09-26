4929c1651c8d8fe05a11ed6cfe315d2f4ce63d1b
JR <justifiedRecursion@gmail.com>
2021-09-26 17:32:11 -0400

    Jeffery's Files complete

---
+++ src/images/Secrets/blatant_lie/0.js
@@ -0,0 +1,17 @@
+
+export const text = `
+
+Tape 17: Day 71
+Moonlight shines through the windows. I am walking the same road. I enter Janice’s room, and backtrack immediately. I turn on my heel, and rotate twice. I grab a donut on my path to the boss’ room, and peek into the closet.
+
+I enter the boss’ room.
+
+I fill in the sheet again. I flip it.
+
+There’s the tenth question. I know that I should answer “yes”, because that is the correct response to the tenth question. But I shall not respond to it in the expected manner. I shall stop this.
+
+I pick up my pen, and fill in “no”.
+
+
+
+`
