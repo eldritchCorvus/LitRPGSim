64dcc5f85da20e0e5568175d7239c0261dc5f31e
JR <justifiedRecursion@gmail.com>
2021-09-26 16:32:33 -0400

    it already HAD happened!!!

---
+++ src/images/Secrets/dodge_this_moist_pimp/0.js
@@ -0,0 +1,14 @@
+
+export const text = `
+http://www.farragofiction.com/AudioLogs/?passPhrase=cheetoh_timeline
+http://www.farragofiction.com/AudioLogs/?passPhrase=cheetohTimeline13
+http://www.farragofiction.com/AudioLogs/?passPhrase=cheetohTimeline1
+
+Zampanio is anything that calls itself Zampanio. Never forget that. 
+
+You could connect a branch of Zampanio to anything you want. Your own work. Your own passions.
+
+You should.
+
+
+`
