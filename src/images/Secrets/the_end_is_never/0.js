8391511ac79e3fe362f31bffabf4faa1a808c1b3
JR <justifiedRecursion@gmail.com>
2021-09-26 11:42:24 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/images/Secrets/the_end_is_never/0.js
+++ src/images/Secrets/the_end_is_never/0.js
@@ -1,11 +1,51 @@
 
-export const text = `Tape 1: Day 1
-...hey, is this thing on?
+export const text = `
+~~~~~
+Tch. Disappearing forms? This is stupid. Should’ve shocked her instead. Now THAT would’ve sparked her memory. 
 
-I am Jeffery Lowe, an archivist of the Zampan Census Bureau. I am [REDACTED] years old, and as of right now, I am charged with digitizing an old archive.
+...get it? Sparked? Whatever. You can’t even hear me, can you. 
+~~~~
+Oh, great. The victim's a crackpot and your new friends are absolutely fucking useless. This case really is just getting better by the minute. Even then, if he was somehow RIGHT, which he isn't, what does he want you to do, handcuff the ghost?
+~~~~
+The forensics chick is the first one I'm cuffing the moment I am given the chance. I don't care if she's not part of the case, I don't care that she's a cop, and I don't care what you think about it. I'm starting to think she's the reason we've been fucking refilling pens for weeks, and I'm going to shock her once for every pen in that box.
+~~~~
+...
 
-I don’t like people. They are noisy and nosy. Data isn’t noisy, and neither is it nosy. It doesn’t pry at you with questions. It doesn’t demand time, it only takes it. At least my coworkers understand that I don’t want to be bothered by them, and I don’t bother them in return.
+Him. He should keep talking. He needs to keep talking. Zap him if he stops talking. Dad needs to know everything. 
+~~~~
+God damn it, you idiot. Zapping is for people, not for CAMERAS. She’s going to find you out, and then we’ll have to—
 
-My coworker left for a vacation a week ago. Sometimes they leave emails I have to respond to. I don’t mind that much. At least it’s not in-person.
+Wait, no. She was the one who brought out that tazer earlier, wasn’t she? Maybe... 
+
+No, that means she would ESPECIALLY notice. Or not? Fuck, kid. You’re on your own.
+~~~~
+A memory core? Oh, excellent.
+
+No, seriously. If there is a single being in this shithole town we can trust besides Dad, it's OURSELVES. Then, maybe you don't have to do all the work carrying these fucking loons.
+~~~~
+You have her in your hand. You have her in your fucking HAND. IT WOULD BE SO EASY TO SHOCK HER. OH MY GOD JUST ZAP HER ALREADY.
+~~~~
+WARRANT
++ war·​rant. wȯr-ənt.
+Definition of WARRANT:
+- a precept or writ issued by a competent magistrate authorizing an officer to make an arrest, a seizure, or a search or to do other acts incident to the administration of justice.
+
+...oh, huh? Oh, don't mind me. Just looking through our FILES on POLICE TERMS. You know, because food isn't an integral part of the administration of justice, or fucking, whatever. 
+
+Who am I talking to? I KNOW you can't even fucking hear any of that, and Dad accidentally locked all those files, so only I can open them. Shouldn't be a problem. You should know that by now.
+
+...you do KNOW what a warrant is. Right?
+
+You don't. Of course you don't. Fuuuuck me.
+~~~~
+...
+
+No. The meme of the soul can’t be WIPED. Dad’s work can’t be WIPED. This is bullshit. Someone must’ve DONE something to it. Slammed it, broke it inside, what the fuck ever. Someone is onto us. Someone is onto US. We’re going to find whoever erased the core and we will HAND them over to the judge beaten within an INCH of their MISERABLE FUCKING LIFE. 
+~~~~
+...thanks, dad. 
+~~~~
+God DAMN IT, kid. The perp gets away, this bartender is bullshit, this bar is SWARMING with criminals. These two fuckers aren't doing anything. ANYTHING. Focus. FOCUS.
+~~~~
+
 
 `
---
+++ src/images/Secrets/the_end_is_never/1.js
@@ -0,0 +1,53 @@
+
+export const text = `
+~~~~
+God DAMN IT, kid. The perp gets away, this bartender is bullshit, this bar is SWARMING with criminals. These two fuckers aren't doing anything. ANYTHING. Focus. FOCUS.
+~~~~
+It's so simple. It's so fucking simple and you don't even SEE it. All you have to do is follow that motherfucker. It's so...
+
+...wait a god damn minute. Can you... what the hell?
+
+...holy shit you can actually hear me. I can't-- I haven't-- 
+
+Okay, not the time for ME to freak out, now. Hey, asshole! Good to hear you. Be you? The details are really not important here. The IMPORTANT THING is that's a perp you're letting get away. 
+
+What the fuck are you waiting for?! Do you think the kleptomaniac and the drunkard are going to do it? If anyone's going to stop a criminal here, it's YOU.
+
+Go, go, go!
+~~~~
+Aaaand we're back to the slaughterhouse. Fan-fucking-tastic. 
+
+Think I should be angrier-- nope, no, I an extremely fucking angry about being back here. Not like it matters. Perp wasn't even caught, dude just ran off. Barely got any info out of that.
+
+Fuuck. I'm... sorry? I can't possibly be sorry. Do I seriously think I FAILED you? That is actually impossible, you don't even know what you are DOING. 
+
+Ugh. Whatever. Carry on, etcetera.
+~~~~
+Seriously?
+
+FUCKING SERIOUSLY?
+~~~~
+Is it, now?
+
+- Find npc.strangeTallRussianMan
+- No matches were found.
+
+Huh. No, I guess we ARE on the same page on that one.
+~~~~
+See, now this is the part where I tell you this bitch is suspicious. I am throwing my feet on the fucking wall right now, leaning back and shit. Seriously. The one person who could possibly get into this guy's house, where we found a scrawled note about them coming for it, all ominous-like? Rando sources, OBVIOUSLY CREEPY FUCKING RUSSIAN GUY, suddenly very okay with being outbid for the book or whatever after they're DEAD, and also we found blueprints of them in that guy's house? USUALLY, I would say cuff that bitch on the spot, but clearly that doesn't work anymore now, or whatever. 
+
+So go off, fucking, I don't give a shit.
+~~~~
+...say, not like you ARE listening anyway, but how come a bunch of disappearing magic artifacts leave huge fuckoff pentagram signs that burn thirty feet deep into wood? I thought the whole point is they were about erasure? Leaving a huge mark that shows you exist seems kind of, I dunno, really fucking stupid. 
+
+...that. That is not a metaphor. By the by. This is NOT some kind of call for help. I leave a thirty foot fuckoff mark because some people just deserve to be zapped, and that is final.
+
+Not like. You would KNOW that last part. You never remember the whole zapping bit.
+
+Ugh. This shit is getting to me again. Watt logging out.
+~~~~
+Oh shit. Here we go again.
+~~~~
+Why'd the fucking lights go off.
+
+`
