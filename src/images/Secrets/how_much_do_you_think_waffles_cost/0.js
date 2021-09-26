c9ca79441fa07bb31ba4141d22a0ebbe26be606f
JR <justifiedRecursion@gmail.com>
2021-09-26 17:12:36 -0400

    chugging along

---
+++ src/images/Secrets/how_much_do_you_think_waffles_cost/0.js
@@ -0,0 +1,12 @@
+
+export const text = `
+
+Tape 11: Day 46
+Fuck, I really can’t afford quitting my job. This means I’m staying. The boss doesn’t parse what’s going on and finds it practical jokes. He says he can’t allow me to be let go and we’re going to be short-staffed if this goes on.
+
+I looked over Quinn’s again, and found a floor plan of a room. I copied it into a notebook. It looks like a generic room, with an entry pointing towards the coffee stain.
+
+I also found the seventh instance of my response, with the question and my response being filled in. Okay, this isn’t funny. I’m going to bring this up with the boss tomorrow.
+
+
+`
---
+++ src/images/Secrets/scanlations/0.js
@@ -0,0 +1,13 @@
+
+export const text = `Tape 7: Day 25
+Found a Jeffery Lowe. I initially thought it’s a namesake, but all obverse responses are accurate to who I am.
+
+It’s me. What the fuck?
+
+There is a coffee stain, with a light pencil scratch in the center. It better be a practical joke, and whoever is responsible better own up to it.
+
+There are no questions on the reverse. I don’t know what I could have expected.
+
+I asked Clarence, who is the only person who goes to my room anymore, and he called it a very accurate replica. I swear to fucking god if it’s Clarence I am throwing him out of the window.
+
+`
