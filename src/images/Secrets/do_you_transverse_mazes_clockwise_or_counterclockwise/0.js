b2331342138e38ae50070b2b861d24a821d99aec
JR <justifiedRecursion@gmail.com>
2021-09-26 17:12:36 -0400

    chugging along

---
+++ src/images/Secrets/do_you_transverse_mazes_clockwise_or_counterclockwise/0.js
@@ -0,0 +1,14 @@
+
+export const text = `
+
+Tape 12: Day 47
+Boss fell ill with acute pneumonia. Guess it’s up to me to dig this up.
+
+Clarence got sent abroad for whatever reason. Some problems they can’t fix remotely. Then it’s gonna be the quarantine.
+
+His copy asked him about exposure to the disease, and there’s another ID and another room. I’m arranging these together and they...well, they don’t fit, but there’s some place between them to let them fit.
+
+I had to leave my area to get donuts. Damn, my entire diet is coffee and donuts at this point. This can’t be good. I should start eating more veggies if I don’t want to have heart problems.
+
+
+`
