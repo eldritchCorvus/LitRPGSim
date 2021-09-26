ccaa6f2999a46d2657ca99c04f7f16890e89ee71
JR <justifiedRecursion@gmail.com>
2021-09-26 17:32:11 -0400

    nidhogg already had corrupted the universe after all!!!

---
+++ src/images/Secrets/meteor_shower/0.js
@@ -0,0 +1,15 @@
+
+export const text = `
+
+Tape 15: Day 69
+The office is a maze. Of course it’s a maze.
+
+It took me forever to notice that the IDs trace a path through the rooms. I just need to draw it and walk it counterclockwise, because I traverse mazes counterclockwise.
+
+I need to wait for everyone to leave. Not just feel like anyone is here. I need to make sure nobody sees me, because I will need to break into the director’s room.
+
+I’m doing it today. I just need to wait for Nate to go home already. Dude has always been pretending he’s being overworked and I fucking hate him for this since he doesn’t do jack shit.
+
+
+
+`
---
+++ src/images/Secrets/verified_fact/0.js
@@ -0,0 +1,16 @@
+
+export const text = `
+
+Tape 16: Day 70
+Yesterday, I walked the path I traced through the archives and picked the boss’ lock. I found a blank census sheet, with a tea stain.
+
+What a stupid joke. It’s him. Of course it’s him. Of course he’s been gaslighting me and everyone else.
+
+I just filled it out. All questions on the obverse, nine on the reverse. I still don’t know what the tenth is.
+
+He came to work today, and I confronted him about this. He said he has no idea what’s going on and that he hates coffee.
+
+Of course he’d lie to my face. I’m going to do it again.
+
+
+`
