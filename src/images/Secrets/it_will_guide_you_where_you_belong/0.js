793b0b6b2e64fe0729997b9a97415c6a7e66cebf
JR <justifiedRecursion@gmail.com>
2021-09-26 16:32:33 -0400

    working on secrets :) :) :)

---
+++ src/images/Secrets/it_will_guide_you_where_you_belong/0.js
@@ -0,0 +1,15 @@
+
+export const text = `
+Tape 3: Day 16
+It’s been more than two weeks! I am roughly halfway through — nevermind, it’s only the first drawer. I hate these drawers.
+
+When I clock out at five, I have to make notes about my progress. I think it looks steady enough, for now? I just need to keep copying data and I’ll make it through, eventually. 
+
+Clarence showed up in my room and offered me donuts. I took one. He didn’t do that before. I don’t like to be disturbed but a caramel donut is a caramel donut, and at least he isn’t too talkative.
+
+I stayed overtime today because I spent too much time on my break and I’m falling behind my quota. The boss won’t be salty but it matters to me. The quota is, after all, self-imposed.
+
+
+
+
+`
