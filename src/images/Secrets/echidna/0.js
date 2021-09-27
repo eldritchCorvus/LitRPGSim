9e1abe1080851e4d1b2e994477bbb273ae18ad89
JR <justifiedRecursion@gmail.com>
2021-09-26 22:24:32 -0400

    the echidna already existed

---
+++ src/images/Secrets/echidna/0.js
@@ -0,0 +1,25 @@
+
+export const text = `
+	Music.
+        	Wild, shrill, and fast.
+        	Instruments unknown.
+        	Daemons of tubes screamed.
+        	Daemons of hollow expanses bellowed.
+        	Daemons of razor thin edges cackled.
+        	Music.
+        	The center widened every more.
+        	The maelstrom now a smooth ring.
+        	Through it I saw a face.
+        	A laughing face.
+        	A face I recognized.
+        	I came to with a start, curled up in a ball on the deck, clutching my head. I felt something in one of my hands. I look at it, the small bottle of cheap beer so tame it couldn’t get a featherweight drunk. It was completely empty. I cursed and whipped it into the barely organized wall of cables. It momentarily tangled in them before bouncing to the ground and rolling along the slightly uneven deck until coming to a stop in the corner with a small impact.
+I shakily stood. I was terrified. I stumbled along the access corridor, trying to break into a run but never finding the footing to do so. I made my way from there down a main corridor.
+Eventually I ran into another pirate. He threw me off of him and I fell to the deck before scrambling back up.
+“Where the fuck have you been? We thought you were fucking dead! Fucking hell what is that smell? You goddamn reek, what kinda shit did you hide away? Holy fuck I might just get drunk sniffin’ you fucking hell.”
+“T-t-t-the boy! H-h-it’s… it’s… we- we need to… we’ve, uh, too late. All we can do now is uh, uh…”
+“Goddamn stop stuttering you drunk motherfucker. Fuckers have been dying left and right and all you can think of is the captain’s last fucktoy? I haven’t even seen him since before the captain died.”
+As I was attempting to collect my thoughts, the pirate vanished. In his place stood the boy. He laughed at me. The sound echoed down the corridor and shook my bones.
+I screamed and tried to get away. But I couldn’t turn to run, I couldn’t look away! All I could do was fall on my ass like a fucking idiot and half-heartedly crawl blindly backwards. It became me, an exact copy of me down to the last detail. But it moved in a way I never would, it winked at me with my eyes, and giggled with my voice, before walking away and turning a corner.
+I wanted to shout, to ask questions, maybe just scream some more. But my throat was hoarse, and my breath was ragged. I got back to my feet and ran down the corridor, away from where the boy had gone.
+
+`
---
+++ src/images/Secrets/go_to_zeus_to_plead_for_her_life/0.js
@@ -0,0 +1,17 @@
+
+export const text = `Hey, I just found something that you will love! While I was in the system hub waiting for my transfer to Cypres I decided to go bar hopping. I was low on cash and didn’t want to go through the hell that is transferring savings on an out-of-network station, so I went cheap with it. In some dingy nook-in-the-bulkhead place that was just a bar and a couple tables between some pipes and an emergency bulkhead I found fucking gold. There was this dingy guy leaning on the counter nursing a big cup of some cheap swill.
+Now yes, I know that the general rule in these places is “don’t ask don’t tell” but this poor bastard looked absolutely shell-shocked. Not depressed, or anxious, but properly shell-shocked like he was a grunt on a border gore mudball eating slugs for breakfast. Mega-klick stare and drinking with a shaking hand, the works. Something had obviously terrified him, and there’s always a good story in that, and I know you love good stories. It’s a long one, he went into a lot of detail and let me record it exactly, but trust me it’s worth it.
+So, I buy him some more booze, still just fermented piss but a slightly more expensive brand that what he was drinking. Don’t get excited it was something only available on that station and I don’t remember the name anyway. I was expecting to have to butter’em up, get him to spill the spuds. But no, it was like he was waiting for someone to hear his story. He takes the drink and asks, “What do you want.”
+You could hear the terror in his voice, he was legitimately terrified of something. At this point I felt bad for him, I was about to tell him to just keep the drink and then go somewhere else. But I realized that he wasn’t terrified of me. That was my second hint that this would be great. Now you know that I’m a big guy, if this dude had done something boring like owe some gangster money or get into it with the regime of some mudball he would’ve been on the lookout for strange, big, muscley, handsome men like myself. But it wasn’t me he was afraid of.
+So, I tell him, “What happened to you?”
+For the first time he faces me, and you can tell by his face that he’s seen some shit, and says, “You don’t want to know”
+I couldn’t help myself there, I chuckled.
+“I’m serious”, he says
+I apologize and tell him that everyone says that, and that whatever follows it is rarely worth the severity of the warning. He chugs the rest of the cup, slams it down, “Okay”
+Then he freezes and stops for a moment, “Wait you probably won’t believe my story anyway.”, he shrugs, “Probably for the better.”
+I ask him if I can record it and he says yes. So I set that up and he gets right into it.
+ 
+I was a pirate. Was. I’m not risking that shit anymore. Not sure what I’ll do now. Anyway I was a pirate. Specifically, the helmsman on a retrofitted cargo ship. Under captain Gregor. No last name, that I know of at least. We all just called him, “Captain”. A few times I overheard the first officer call him Gregor. We had been prowling around the Edge, going back and forth between pioneer worlds and the mudballs. Intercepting supply ships bound for the pioneer 
+
+
+`
