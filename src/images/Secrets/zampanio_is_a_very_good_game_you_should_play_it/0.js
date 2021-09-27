246355bb4f1096269beca534d7a84dd93ca65fcb
JR <justifiedRecursion@gmail.com>
2021-09-26 22:24:32 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

---
+++ src/images/Secrets/zampanio_is_a_very_good_game_you_should_play_it/0.js
@@ -0,0 +1,13 @@
+
+export const text = `
+	It lifted its hand from my head and walked around into my view. It sat with both laziness and grace on a console to my left. A man, tall and muscular. His body was thick and powerful. His face was cut like a fine jewel. His eyes shone like a pair of them. His countenance betrayed great experience that lent his unmarred and wrinkle-free face a sense of extreme maturity. Long curly hair fell from his head, and a great bushy beard covered the lower half of his face. He wore the torn remains of a jumpsuit that left his powerful chest and arms in plain view, thick curly hair covering much of those as well. From the curls just above the corners of his forehead emerged two long horns, bending back in an elegant curve. A garland of fruit-bearing vine circled his head.
+“I am a god”, he waved his hand in a dismissive gesture, with a voice to match.
+“W-which one?”, I had never been a religious man myself, but I had heard of multiple religions, all of which contradicted not only each other but themselves.
+His massive chest moved with a sigh, “Ah, it has been far too long since one of my many names has been spoken. You wouldn’t recognize any of them. Just know that I am Madness. I will always be there when the hypocritical nature of man is dressed in the stolen garb of Order and its false “justice” is forced upon you.
+He reached up and took one of the purple fruits growing on his ‘crown’ before popping it into his mouth.
+ 
+The pirate’s story ended there. Obviously, he made it back to port somehow, as he was there. I tried to get more out of him, at least if the station I had found him on was the one he had docked at, but he wouldn’t talk. He just whispered something and took a drink. He ignored me after that.
+Well I had found the story quite entertaining! I hope you do as well. I hope this finds you well. I write as I travel out to Cypres, once I conclude my business there, I’ll be heading right back, see you soon!
+
+
+`
