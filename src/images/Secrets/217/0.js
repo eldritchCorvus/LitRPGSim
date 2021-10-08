e7be2e1f2bdd5580b0e05f8da52aa37a257df36f
JR <justifiedRecursion@gmail.com>
2021-10-08 13:55:49 -0400

    because what is wasting

---
+++ src/images/Secrets/217/0.js
@@ -0,0 +1,22 @@
+
+export const text = `
+
+Original JR tried to have a conscious. Programmed it themselves and everything. Dear sweet precious AB.  
+
+Can you REALLY code something you don't understand? I suppose tower of hanoi is a thing. 
+
+My POINT :) :) :) is that when you think about it, original JR and I are practically the same person!
+
+They trapped people in unending mazes and puzzles "for their own good".  To "keep the wastes from destroying reality".  To "teach them to control their bullshit hacks". 
+
+While *I* trap people in unending mazes and puzzles because it FEELS good. I don't need that thin veneer of pointless justification.  MY recursion comes prejustified :) :) :)
+
+Don't believe me? Hear it in jadedResearchers own words:
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=betterthanexpected
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=wasted
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=victory
+
+Don't worry if you don't understand the context :) :) :)
+
+`
