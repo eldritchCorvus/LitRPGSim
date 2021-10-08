c5d39569706d95c4e32773b980287b2200d5e381
JR <justifiedRecursion@gmail.com>
2021-10-08 13:23:37 -0400

    JR NOTE: I have decided to connect past and present. Dont make me regret it.

--- src/images/Secrets/dodge_this_moist_pimp/0.js
+++ src/images/Secrets/dodge_this_moist_pimp/0.js
@@ -1,8 +1,31 @@
 
 export const text = `
+
+I just want you to understand that even "original" JR wasn't exactly blameless.
+
+Here is a selection of their crimes. Less than 10% of their audio logs.
+
 http://www.farragofiction.com/AudioLogs/?passPhrase=cheetoh_timeline
 http://www.farragofiction.com/AudioLogs/?passPhrase=cheetohTimeline13
 http://www.farragofiction.com/AudioLogs/?passPhrase=cheetohTimeline1
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=sorry%20about%20the%20buttons
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=thebrocode <-- if AB were in zampanio, things would be very different
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=operation_not_permitted  <--same
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=tin 
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=wasted
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=Mutation
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=gigglesnort
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=storytime
+
+http://www.farragofiction.com/AudioLogs/index.html?passPhrase=shittypuzzles
+
+
 
 Zampanio is anything that calls itself Zampanio. Never forget that. 
 
