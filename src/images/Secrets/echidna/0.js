441b498642e994cb1894bfe49fa742d448966430
JR <justifiedRecursion@gmail.com>
2021-11-02 21:32:17 -0400

    i have all SORTS of ideas for what i can do to the insides of this abomonation

--- src/images/Secrets/echidna/0.js
+++ src/images/Secrets/echidna/0.js
@@ -17,7 +17,9 @@ I shakily stood. I was terrified. I stumbled along the access corridor, trying t
 Eventually I ran into another pirate. He threw me off of him and I fell to the deck before scrambling back up.
 “Where the fuck have you been? We thought you were fucking dead! Fucking hell what is that smell? You goddamn reek, what kinda shit did you hide away? Holy fuck I might just get drunk sniffin’ you fucking hell.”
 “T-t-t-the boy! H-h-it’s… it’s… we- we need to… we’ve, uh, too late. All we can do now is uh, uh…”
-“Goddamn stop stuttering you drunk motherfucker. Fuckers have been dying left and right and all you can think of is the captain’s last fucktoy? I haven’t even seen him since before the captain died.”
+“Goddamn stop stuttering you drunk motherfucker. Fuckers have been dying left and right and all you can think of 
+
+I haven’t even seen him since before the captain died.”
 As I was attempting to collect my thoughts, the pirate vanished. In his place stood the boy. He laughed at me. The sound echoed down the corridor and shook my bones.
 I screamed and tried to get away. But I couldn’t turn to run, I couldn’t look away! All I could do was fall on my ass like a fucking idiot and half-heartedly crawl blindly backwards. It became me, an exact copy of me down to the last detail. But it moved in a way I never would, it winked at me with my eyes, and giggled with my voice, before walking away and turning a corner.
 I wanted to shout, to ask questions, maybe just scream some more. But my throat was hoarse, and my breath was ragged. I got back to my feet and ran down the corridor, away from where the boy had gone.
