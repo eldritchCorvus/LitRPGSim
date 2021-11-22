38c946a72d52b4e0fb0af3e9cd9a1053a2907fe6
JR <justifiedRecursion@gmail.com>
2021-11-21 20:34:24 -0500

    timeline restored

---
+++ src/Screens/WalkAround/NAMStorage.ts
@@ -0,0 +1,15 @@
+export const ficlets = [
+    `Melon.
+
+That word lingers at the tip of your tongue, in the back of your mind. 
+
+You swear it nearly leaves your mouth, sometimes. In the roots of the potted plant near the kitchen; in the threads of some stray gold wires. In the fruit that the Closer hoards so mercilessly, and the Flower Chick’s long coat. In Tyrfing’s glare, intent with resolution. In the Killer’s strange insistence of taking you, and you alone. 
+
+A spark of something, just waiting to be tapped into. Just waiting for you to remember.
+
+It chokes you up just thinking about it, in ways a ghost shouldn’t and a robot couldn’t. For a second, you exhale in a way you haven’t felt before. Not a staged breath, no. This is no simulation. Something real.
+
+What was it?
+
+Why can’t you remember?`
+];
