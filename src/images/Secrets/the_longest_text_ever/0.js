48e981185994ca0adeba93c82599cb01f4f8ee07
JR <justifiedRecursion@gmail.com>
2021-12-11 10:08:55 -0500

    init

--- src/images/Secrets/the_longest_text_ever/0.js
+++
@@ -1,9 +0,0 @@
-
-export const text = `Tape 5: Day 22
-Clarence laughed it off and pointed at the data. The census is indeed dated two hundred years ago. Then he offered me another caramel donut, and suggested that I leave my area more often.
-
-It isn’t funny, I did not forge the response. It’s authentic. He said that it might be a coincidence. Maybe it is a coincidence, but maybe it is not. I should just put it aside for now and focus on my work.
-
-I’m running out of coffee capsules. I need to get more for the machine. I need coffee to function, sadly. I don’t even work much…
-
-`
