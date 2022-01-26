faa40861487360bbb95e4a6fdc77891d0509b1fe
JR <justifiedRecursion@gmail.com>
2022-01-25 20:04:33 -0500

    rough drafts

--- src/Screens/WalkAround/Chat/HelpDesk/CEOChats/trovetextravaganza.ts
+++ src/Screens/WalkAround/Chat/HelpDesk/CEOChats/trovetextravaganza.ts
@@ -2,7 +2,6 @@ import { TBDChatType } from "../../TBD/PreCoffinChats";
 
 export const ttChats:TBDChatType[] = [];
 
-//TODO: ceo and cfo discussing wasting, loss pass, the intern, the birbs
 ttChats.push({timestamp: new Date(1992, 4,1,16,24 ),lines: `CEBro: WE DID IT
 CEBro: WE FINALLY HAVE REJOINED THE MODERN ERA
 CEBro: CHAT CLIENTS FOR ALL
@@ -45,10 +44,68 @@ CEBro: CAN YOU FIGURE OUT HOW MUCH MONEY WE'LL NEED TO SPIN OFF A SHELL COMPANY?
 troveTextravaganza: that i can certainlyyyy do
 troveTextravaganza: but you owe me a pizza because it's super late
 CEBro: DEAL`})
+
+
+ttChats.push({timestamp: new Date(1996, 5,2,9,1 ),lines: `troveTextravaganza: wanda
+troveTextravaganza: whhhhhhhhhhhy am i seeing on the news
+troveTextravaganza: the fact that you are going to represent the EYE KILLER in court
+CEBro: BECAUSE ITS HILARIOUS
+troveTextravaganza: i'll call the PR team...`});
+
+
 ttChats.push({timestamp: new Date(1999, 12,31,23,54 ),lines: `CEBro: I HAVE ALWAYS BEEN CURIOUS AT THE IDEA OF A REVERSE BANG AND AFTER REALIZING THAT MY THOUGHTS WOULD ONLY BECOME REALITY IF I MADE IT HAPPEN, I HAVE DECIDED TO CREATE THIS EVENT MYSELF
 CEBro: BEFORE I GET TOO FAR INTO THIS PROCESS THOUGH I AM LOOKING FOR FEEDBACK
 troveTextravaganza: why call it the reverse bang it sounds so lame
 CEBro: BECAUSE ITS LIKE THE BIG BANG IN REVERSE, I SAW IT ON TUMBLR IT'LL BE HILARIOUS
 CEBro: AND IF WE CAN'T STOP IT
 CEBro: WE MIGHT AS WELL MAKE IT AS FUNNY AS POSSIBLE`});
+
+ttChats.push({timestamp: new Date(2000, 4,1,22,12 ),lines: `CEBro: BRO
+troveTextravaganza: happy birthay wanda
+troveTextravaganza: the big 110010 +/- x
+CEBro: BRO
+CEBro: I JUST GOT BACK FROM THAT SHITTY THEME PARK
+troveTextravaganza: was it fun??
+CEBro: NO
+CEBro: IT WAS TERRIBLE
+CEBro: DID YOU KNOW THEY GOT RID OF THE LINES?
+CEBro: YOU JUST WALK RIGHT ONTO THE RIDE
+CEBro: AFTER GETTING A LITTLE TICKET
+troveTextravaganza: omg
+troveTextravaganza: is this going to be that 'open office plan' all over again?
+CEBro: NO
+CEBro: ITS WORSE
+CEBro: WHO GOES TO A THEME PARK TO JUST WALK STRAIGHT ONTO RIDES
+CEBro: WHERES THE MYSTIQUE
+CEBro: WHERES THE CONFUNSION
+CEBro: WHERES THE CREEPING FEAR THAT MAYBE THE LINE YOU'RE IN IS GOING TO SOMETHING BESIDES WHAT YOU THOUGHT IT DID
+troveTextravaganza: yes yes, wheres the whole 'being lost' vibe you love so much
+troveTextravaganza: what are you gonna do, like, replace them in the next loop?
+CEBro: GIVE YOURSELF A RAISE
+CEBro: THATS GENIUS
+CEBro: OR HELL
+CEBro: WE CAN PROBABLY AFFORD TO JUST BUY THEM AND FIX THEIR SHITTY SYSTEM
+troveTextravaganza: wanda, babe
+troveTextravaganza: im the chief financial officer
+troveTextravaganza: i don't need to give myself a raise
+troveTextravaganza: but i DO need to tell you that if you pull money out of your ass those poor auditors are going to show up again
+CEBro: NAH BRO ITS FINE
+CEBro: I HAVE A PLAN FOR THIS
+troveTextravaganza: why does this already upset me
+CEBro: WHAT DO YOU REMEMBER ABOUT CRYPTOCURRENCY?`})
+
+ttChats.push({timestamp: new Date(2000, 4,1,22,12 ),lines: `CEBro: http://knucklessux.com/LossPass/
+troveTextravaganza: thanks i hate it
+troveTextravaganza: wait you can't host it there
+troveTextravaganza: the local storage doesn't save
+CEBro: SHIT
+troveTextravaganza: you have to host it on that weirdos site
+CEBro: SHIT
+CEBro: CAN YOU MESSAGE THEM?
+CEBro: I AM JUST NOT A FAN OF INTERACTING WITH JR
+CEBro: THEIR SMILE CREEPS ME OUT
+troveTextravaganza: sure thing
+troveTextravaganza: but you know they are absolutely gonna slap a "Sim" on the end there
+CEBro: WHATEVER
+CEBro: THATS A PROBLEM FOR MARKETING TO SOLVE`})
 
