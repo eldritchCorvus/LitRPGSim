be940ab88981b3cd5b5f51ae6220e2e96feec48a
JR <justifiedRecursion@gmail.com>
2022-01-29 12:58:59 -0500

    more chats

--- src/Screens/WalkAround/Chat/HelpDesk/CEOChats/closer.ts
+++ src/Screens/WalkAround/Chat/HelpDesk/CEOChats/closer.ts
@@ -2,8 +2,69 @@ import { TBDChatType } from "../../TBD/PreCoffinChats";
 
 export const tcChats:TBDChatType[] = [];
 //the closer and ceo discussing fruit baskets, customer satisfaction targets, metrics, etc
-tcChats.push({timestamp: new Date(1994, 8,13,16,24 ),lines: `CEBro: HEY
-theCloser: hey
+tcChats.push({timestamp: new Date(1994, 8,24,16,24 ),lines: `CEBro: LOL
+CEBro: DID YOU REALLY SEND THE INTERN A FRUIT BASKET?
+theCloser: Ma'am, it is only good business to welcome and compensate employees in the ways that we would like to be treated.
+theCloser: Therefore, I have provided a fruit basket, yes.
+theCloser: Would you have preferred me not to? I do not believe the Intern has any preceding food allergies I have not already checked for.
+CEBro: WE'VE HAD 13 SEPERATE INTERNS IN THE PAST 20 YEARS
+CEBro:  EXACTLY ONE HAS GOTTEN A FRUIT BASKET
+CEBro: AT LEAST BEFORE BEING BROUGHT ON PERMANENTLY
+CEBro: IM JUST SAYING
+CEBro: ITS NOT LIKE YOU TO PICK FAVORITES LOL
+theCloser: Fair enough. I will concede I prefer not to engage in the concept of favorites, especially when they are... what would you call it? Fresh off the tree.
+theCloser: I will correct myself, then. I have taken notice of the lengths of which you have presented interest in this individual. 
+theCloser: Pushing the wagon along, as you will.
+CEBro: ...
+CEBro: YEAH OKAY FAIR
+CEBro: KEEP SHOWERING THE BOI WITH PRESENTS
+CEBro: ITS FUNNY
+CEBro: BUT DON'T FREAK HIM OUT
+CEBro: CAN'T HAVE HIM QUITTING ON US
+theCloser: Is there a reason I would freak him out?
+theCloser: Well. Besides the usual.
+theCloser: I promise that, to my knowledge, most get quickly used to static in their landlines.
+theCloser: Except for poor John, I suppose, but to his defense, he already had problems with ear ringing. I would rightly be paranoid at that point.
+CEBro: THE INTERNS ALWAYS BEEN KINDA NEUROTIC
+CEBro: JUST BACK OFF IF I TELL YOU TOO, OKAY?
+theCloser: Note taken. I will make sure to keep an eye out.
+theCloser: Is there anything else you wish to discuss?
+CEBro: NAH BRO
+CEBro: AS YOU WERE`})
+
+tcChats.push({timestamp: new Date(1995, 1,12,08,58 ),lines: `CEBro: SORRY TO INTERUPT YOUR LUNCH BREAK
+CEBro: BUT THE INTERN JUST FOUND JEPE CAMPING OUT IN THAT MEETING ROOM
+CEBro: CAN YOU TAKE CARE OF IT?
+theCloser: Will do.
+theCloser: Is Jepe continuing their crusade for unearned fruit still?
+CEBro: THEY'VE DECIDED THAT IF WE WON'T RECOGNIZE THEIR SKILL
+CEBro: THEY WILL JUST DECLARE THEMSELF PROMOTED
+CEBro: FRUIT AND ALL
+theCloser: Pathetic.
+theCloser: I would not dare engage in insubordination, Ma'am, but I will admit I am dangerously close to deciding to deal with the nuisance myself.
+theCloser: To say they deserve fruit, of all things. The nerve of some...
+CEBro: BRO IF YOU WANNA DELEGATE
+CEBro: DELEGATE
+CEBro: JUST FIND A WAY TO KEEP THEM OUT OF THE WAY OF THE INTERN
+CEBro: ALSO
+CEBro: SOMETHING WEIRD WAS GOING ON IN THAT ROOM
+CEBro: FIND OUT WHAT IT IS
+theCloser: I... can certainly keep them out of the way, yes. I will do as such, then.
+theCloser: Will report on anything else I happen to find out in that room, as well.
+CEBro: I'LL LEAVE YOU TO IT THEN`})
+
+tcChats.push({timestamp: new Date(1995, 1,12,08,58 ),lines: `theCloser: Ma'am, I'm reporting back with the information you had requested.
+theCloser: Jepe has been successfully evacuated from the perimeter. They will have a significantly harder time re-entering that room without me knowing about it.
+theCloser: Besides that, the report on the disturbance is as follows: There was a green-cloaked individual loudly ranting about some cereal from a brand commonly known as 'Lucky Charms', as well as assembling a board of presumed relationships in the office, as well are some other unknowns.
+theCloser: It includes, in no particular order: Yourself, the CFO, some robot I have identified as an obscure TTRPG character, the Eye Killer, for some reason, and myself, among others.
+theCloser: I have only managed to decipher that they believe I have some sort of crush on the CFO, which I discovered because they were yelling about it.
+theCloser: It's a strange assumption and wholly untrue, but that is hardly the point.
+theCloser: That should be all the immediately relevant information.
+CEBro: I SHOULD HAVE KNOWN
+CEBro: MAKE SURE THAT GREEN ASSHOLE DOESN'T SNEAK BACK IN HERE
+CEBro: IS THERE no  LAYER OF REALITY THEY WON'T FUCK WITH?
 `})
+
+
 
 
