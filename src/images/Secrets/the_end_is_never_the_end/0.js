7a3c39b90febc1081e63abdef5dfee978ac7a38b
JR <justifiedRecursion@gmail.com>
2022-02-01 11:23:11 -0500

    more

--- src/images/Secrets/the_end_is_never_the_end/0.js
+++ src/images/Secrets/the_end_is_never_the_end/0.js
@@ -8,4 +8,12 @@ we have:
  "soulful brooding male hero meets equally brooding female villain that teaches him i mean. nothing concrete he's just kind of scared the whole time"
   and
  "brooding but also literally a cop with no restraints male hero meets ikea schedule manager that teaches him to chill the fuck out while she tries to figure out if he actually is the previous soulful brooding male hero"
-`
+
+~~~~~~~~~~~~~
+a: see i love this connection weve found between two chars who never have yet shared a scene together 
+
+b: i mean it only makes sense
+b: they're both beaten junkyard dogs
+b: its just one of them is being trained by one of those dog agencies into a dog a family can adopt and the other one ran into the forest and eats peoples cats
+
+ `
