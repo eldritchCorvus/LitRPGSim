3493a6caab246cbe10ac38929bba53aa849d7f56
JR <justifiedRecursion@gmail.com>
2022-02-17 09:17:22 -0500

    herald meme

--- src/Layer1.tsx
+++ src/Layer1.tsx
@@ -9,6 +9,7 @@ function Layer1() {
     <Content>
       TRUTH.
       <ul>
+        <li>jr can't walk over benches</li>
         <li>(but a second jr and false floor  behind mirror. second jr is in a hidden room and uses the opposite of the up/down that jr uses)</li>
         <li>if mirrors are real , jr can think about themself</li>
         <li>further you get, the faster the rot becomes. til you get to the point where even the present is rotting</li>
@@ -19,6 +20,14 @@ function Layer1() {
         <li>You can move backwards to revisit previous Truths, but the Rot is coming.</li>
         <li>rot makes the room gradually darker, then starts glitching it out (try out the text effects). spooky images start phasing in if you go far enough back. eventually, you can't go back anymore. jr refuses.</li>
         <li>In addition to corrupting the room, the Rot does a RotX cipher on it. For nearly no reason other than to make things harder on everyone. Code rot makes it harder and harder to debug wigglersim</li>
+
+Object Ideas:
+* flaming chicken
+*moon (maze madness and lunar colony)
+* silver cat
+* pigeons
+
+
 
 * train car object has: 
     * central object
