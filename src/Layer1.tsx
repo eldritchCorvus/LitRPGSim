ad13e7f94bc520454878015310f13135008c1a5b
JR <justifiedRecursion@gmail.com>
2022-02-26 16:54:52 -0500

    adding notes

--- src/Layer1.tsx
+++ src/Layer1.tsx
@@ -9,34 +9,21 @@ function Layer1() {
     <Content>
       TRUTH.
       <ul>
-        <li>if mirrors are real , jr can think about themself</li>
         <li>you never know what bits of the past leak into the present</li>
-        <li>In each car is a single object. Investigating it gives you my surface thoughts on it.</li>
         <li>In Layer 2, Truth the character discusses the object as well on a philosophical level.</li>
-        <li>In Layer 3 (console), I mention my coding thoughts.</li>
-        <li>In Each Car, reveal one Truth, at random. (examples, why things are in React not dart, the nature of the maze that is the code base, my experience with corporate life, the sources of various characters)</li>
-        <li>rot makes the room gradually darker, then starts glitching it out (try out the text effects). spooky images start phasing in if you go far enough back. eventually, you can't go back anymore. jr refuses.</li>
         <li>In addition to corrupting the room, the Rot does a RotX cipher on it. For nearly no reason other than to make things harder on everyone. Code rot makes it harder and harder to debug wigglersim</li>
 
 Object Ideas:
 * flaming chicken
+*why things are in React not dart
 *moon (maze madness and lunar colony)
 * silver cat
+*the sources of various characters
+* my experience with corporate life
+*the nature of the maze that is the code base
 grape pie:  http://farragofiction.com/GrapePie/  (link in the console layer)
 * pigeons
 *the 9 artifacts
-
-
-
-* train car object has: 
-    * central object
-    (rot is NOT part of it, its true random. the rot of chaos)
-* in the center of each room is an object, stored in ZampanioHotlink
-* if jr walks up to the object, you get: 
-     * jr within's surface thoughts on it as a thought bubble
-     * truth's "thoughts" on it in the background
-     * jr-who-was-coding's thoughts on it in the console
-     * unrelated truths in the GoshShouldYouTrustThis
       </ul>
       
       <img src="http://farragofiction.com/ZampanioGoshShouldYouTrustThis/this_file_is_red.png"/>
